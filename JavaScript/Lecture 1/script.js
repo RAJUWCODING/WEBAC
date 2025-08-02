3e266ff5d4c481a728d4dd2b626ae09ea06441da
RAJUWCODING <rajuwcoding@gmail.com>
2025-08-02 16:17:22 +0530

    Variables & Datatype JS Learning

---
+++ JavaScript/Lecture 1/script.js
@@ -0,0 +1,206 @@
+// alert("Raju Kumar"); 
+// alert("Hello JS");
+
+// console.log('Hello World');
+// console.log("Hello World");
+
+// fullName = "Abhina Khna";
+// console.log(fullName);
+
+
+// age = 24;
+// console.log(age);
+
+// price = 74.65;
+// console.log(price);
+
+// w = null;
+// console.log(w);
+
+// y = undefined;
+// console.log(y);
+
+// isFollow = false;
+// console.log(isFollow);
+
+// isPass = true;
+// console.log(isPass);
+
+
+//  Variable names are case sensitive; “a” & “A” is different.
+// fullName = "Rajan";
+// fullname = "Yatin" ;
+// console.log(fullName);
+// console.log(fullname);
+
+// Only letters, digits, underscore( _ ) and $ is allowed. (not even space).
+// is_pass = true;
+// console.log(is_pass);
+// $ = 89.63;
+// console.log($);
+
+// Reserved words cannot be variable names.
+// console = "Sanjay"
+// console.log(console);
+
+// fullName - Camel case 
+// isFeaollow
+// totalPric
+
+
+// let fullName = "Tanmay Bhat";
+// console.log(fullName);
+
+// let totalPric = 55;
+// console.log(totalPric);
+
+// let age = 32 ;
+// console.log(age);
+
+// var :   Variable can be re-declared & updated. A global scope variable.
+// var age = 23;
+// var age = 55;
+// var age = 64;
+// console.log(age);
+
+// let  :   Variable cannot be re-declared but can be updated. A block scope variable.
+// ?re-declared
+// let price = 64.4;
+// let price = 54.2;
+// let price = 84.6 ;
+// console.log(price);
+
+// ?updated
+// let price = 64.4;
+// price = 54.2;
+// price = 84.6 ;
+// console.log(price);
+
+// ?re-declared
+// let fullName = "Yatin";
+// let fullName = "Aman";
+// let fullName = "Suraj";
+// console.log(fullName);
+
+// ?updated
+// let fullName = "Yatin";
+// fullName = "Aman";
+// fullName = "Suraj";
+// console.log(fullName);
+
+// const :  Variable cannot be re-declared or updated. A block scope variable
+// ?re-declared
+// const age = 26;
+// const age = 34;
+// const age = 41;
+// console.log(age);
+
+// ?updated
+// const age = 33;
+// age = 42;
+// age = 53;
+// console.log(age);
+
+
+// ? block 
+// {
+//     let egg = 4;
+//     console.log(egg);
+// }
+// {
+//     const laptop = 12;
+//     console.log(laptop);
+// }
+
+// // # Primitive Data Type 
+// 1 Number Data Type;
+// let price = 64.4;
+// price = 54.2;
+// price = 84.6 ;
+// console.log(price);
+// const age = 22;
+// console.log(age);
+
+// 2 String Data Type;
+// let fullName = "Yatin";
+// fullName = "Aman";
+// fullName = "Suraj";
+// console.log(fullName);
+// const text = "Hello World";
+// console.log(text);
+
+// 3 Boolean Data Type;
+// let isFeaollow = false;
+// console.log(isFeaollow);
+// const isPass = true;
+// console.log(isPass);
+ 
+// 4 undefined Data Type;
+// let y ;
+// console.log(y);
+
+// 5 null Data Type;
+// let e = null;
+// console.log(e);
+// const g = null
+// console.log(g);
+
+// 6 BigInt Data Type;
+// let p = BigInt("5678");
+// console.log(p); //5678n
+// const o = BigInt("09876");
+// console.log(o); // 9876n
+
+// 7 Symbol Data Type;
+// let w = Symbol("World");
+// console.log(w, typeof["w"]);
+
+// // ? Obkect 
+// const student = {
+//     fullName : "Dipak",
+//     age : 42,
+//     isPass : true,
+//     cgpa : 8.6
+// }
+// console.log(student);
+
+// Change any Variables - const Object Key is Changebal
+// const student = {
+//     fullName : "Dipak",
+//     age : 42,
+//     isPass : true,
+//     cgpa : 8.6
+// }
+// student ["age"] = student ["age"] + 8;
+// console.log(student["age"]);
+
+// // in Number 
+// let ram = 12+3;
+// console.log(ram); // 15
+
+// in String 
+// let ram = "12" + 3;
+// console.log(ram); // 123
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
